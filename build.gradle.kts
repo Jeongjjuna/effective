39e5e1fe032973a9130528faf3f91086aa2537ee
jeongjihun <wlgns2234@naver.com>
2024-10-10 03:49:32 +0900

    [Effective Kotlin] 요약

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.9.24" // JVM에서 코틀린을 사용하기 위한 기본 플러그인
+    id("java") // JVM 자바를 사용하기 위한 기본 플러그인
+}
+
+kotlin {
+    /**
+     *  코틀린을 컴파일 할 때, JDK21 버전의 바이트 코드로 컴파일하도록 해줌.
+     *  실행될 JVM 버전을 명시적으로 지정해준다.
+     */
+    jvmToolchain(21)
 }
 
 group = "yjh"
